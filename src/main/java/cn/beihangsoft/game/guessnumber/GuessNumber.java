b2af4f9f5853cad559294afdf425854fa8cef952
admin <admin@admin-PC>
2012-10-28 16:26:32 +0800

    validate input number

--- src/main/java/cn/beihangsoft/game/guessnumber/GuessNumber.java
+++ src/main/java/cn/beihangsoft/game/guessnumber/GuessNumber.java
@@ -11,16 +11,16 @@ public final class GuessNumber {
 	private final static int numberCount = 4;
 	private final static int tryCount = 6;
 	private String serverNumber;
-	private HashMap<String, Integer> hashMap;
+	private HashMap<String, Integer> hmServerNumber;
 
 	public GuessNumber() {
 		serverNumber = "";
-		hashMap = new HashMap<String, Integer>();
+		hmServerNumber = new HashMap<String, Integer>();
 
-		while (hashMap.size() < numberCount) {
+		while (hmServerNumber.size() < numberCount) {
 			String number = Integer.toString((int)(Math.random() * 10));
-			if (!hashMap.containsKey(number)) {
-				hashMap.put(number, hashMap.size());
+			if (!hmServerNumber.containsKey(number)) {
+				hmServerNumber.put(number, hmServerNumber.size());
 				serverNumber += number;
 			}
 		}
@@ -40,14 +40,14 @@ public final class GuessNumber {
 			throw new Exception("The number MUST BE " + numberCount + " digits!");
 		}
 
-		hashMap = new HashMap<String, Integer>();
+		hmServerNumber = new HashMap<String, Integer>();
 		for (int i = 0; i < numberCount; i++) {
 			String number = String.valueOf(serverNumber.charAt(i));
-			if (hashMap.containsKey(number)) {
+			if (hmServerNumber.containsKey(number)) {
 				throw new Exception("The number " + number + " is repeated!");
 			}
 
-			hashMap.put(number, hashMap.size());
+			hmServerNumber.put(number, hmServerNumber.size());
 		}
 
 		this.serverNumber = serverNumber;
@@ -62,13 +62,23 @@ public final class GuessNumber {
 			throw new Exception("The number MUST BE " + numberCount + " digits!");
 		}
 
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+		for (int i = 0; i < numberCount; i++) {
+			String number = String.valueOf(serverNumber.charAt(i));
+			if (hashMap.containsKey(number)) {
+				throw new Exception("The number " + number + " is repeated!");
+			}
+
+			hashMap.put(number, hashMap.size());
+		}
+
 		int countA = 0;
 		int countB = 0;
 		for (int i = 0; i < numberCount; i++) {
 			String number = String.valueOf(clientNumber.charAt(i));
 
-			if (hashMap.containsKey(number)) {
-				if (hashMap.get(number) == i) {
+			if (hmServerNumber.containsKey(number)) {
+				if (hmServerNumber.get(number) == i) {
 					countA++;
 				} else {
 					countB++;
@@ -84,7 +94,7 @@ public final class GuessNumber {
 		boolean goal = false;
 		GuessNumber guessNumber = new GuessNumber();
 
-		while (count++ < tryCount) {
+		while (count < tryCount) {
 			System.out.print("Guess the number: ");
 
 			try {
@@ -97,7 +107,10 @@ public final class GuessNumber {
 				if (result.equals("4A0B")) {
 					goal = true;
 					System.out.println("Good job!");
+					break;
 				}
+
+				count++;
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
