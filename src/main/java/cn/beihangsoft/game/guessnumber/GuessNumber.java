ba150c2231230b3585f1b85a83449ba2d0bff2db
admin <admin@admin-PC>
2012-10-28 15:22:30 +0800

    first commit

---
+++ src/main/java/cn/beihangsoft/game/guessnumber/GuessNumber.java
@@ -0,0 +1,112 @@
+package cn.beihangsoft.game.guessnumber;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+/**
+ * @author Zhiyong
+ */
+public final class GuessNumber {
+	private final static int numberCount = 4;
+	private final static int tryCount = 6;
+	private String serverNumber;
+	private HashMap<String, Integer> hashMap;
+
+	public GuessNumber() {
+		serverNumber = "";
+		hashMap = new HashMap<String, Integer>();
+
+		while (hashMap.size() < numberCount) {
+			String number = Integer.toString((int)(Math.random() * 10));
+			if (!hashMap.containsKey(number)) {
+				hashMap.put(number, Integer.valueOf(hashMap.size()));
+				serverNumber += number;
+			}
+		}
+	}
+
+	/**
+	 * Only for test
+	 *
+	 * @param serverNumber
+	 */
+	public GuessNumber(String serverNumber) throws Exception {
+		setServerNumber(serverNumber);
+	}
+
+	public void setServerNumber(String serverNumber) throws Exception {
+		if (serverNumber == null || serverNumber.length() != numberCount) {
+			throw new Exception("The number MUST BE " + numberCount + " digits!");
+		}
+
+		hashMap = new HashMap<String, Integer>();
+		for (int i = 0; i < numberCount; i++) {
+			String number = String.valueOf(serverNumber.charAt(i));
+			if (hashMap.containsKey(number)) {
+				throw new Exception("The number " + number + " is repeated!");
+			}
+
+			hashMap.put(number, Integer.valueOf(hashMap.size()));
+		}
+
+		this.serverNumber = serverNumber;
+	}
+
+	public String getServerNumber() {
+		return serverNumber;
+	}
+
+	public String validate(String clientNumber) throws Exception {
+		if (clientNumber == null || clientNumber.length() != numberCount) {
+			throw new Exception("The number MUST BE " + numberCount + " digits!");
+		}
+
+		int countA = 0;
+		int countB = 0;
+		for (int i = 0; i < 4; i++) {
+			String number = String.valueOf(clientNumber.charAt(i));
+
+			if (hashMap.containsKey(number)) {
+				if (((Integer)hashMap.get(number)).intValue() == i) {
+					countA++;
+				} else {
+					countB++;
+				}
+			}
+		}
+
+		return countA + "A" + countB + "B";
+	}
+
+	public static void main(String[] args) {
+		int count = 0;
+		boolean goal = false;
+		GuessNumber guessNumber = new GuessNumber();
+
+		while (count++ < tryCount) {
+			System.out.print("Guess the number: ");
+
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+				String clientNumber = reader.readLine();
+
+				String result = guessNumber.validate(clientNumber);
+				System.out.println(result);
+
+				if (result.equals("4A0B")) {
+					goal = true;
+					System.out.println("Good job!");
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+
+			System.out.println();
+		}
+
+		if (!goal) {
+			System.out.println("Bad job! The answer is: " + guessNumber.getServerNumber());
+		}
+	}
+}
---
+++ src/test/java/cn/beihangsoft/game/guessnumber/GuessNumberTest.java
@@ -0,0 +1,34 @@
+package cn.beihangsoft.game.guessnumber;
+
+import static junit.framework.Assert.*;
+import org.junit.Test;
+
+public class GuessNumberTest {
+	@Test
+	public void shouldReturn0A0B() throws Exception {
+		GuessNumber guessNumber = new GuessNumber("1234");
+		String result = guessNumber.validate("5678");
+		assertEquals(result, "0A0B");
+	}
+
+	@Test
+	public void shouldReturn0A4B() throws Exception {
+		GuessNumber guessNumber = new GuessNumber("1234");
+		String result = guessNumber.validate("4321");
+		assertEquals(result, "0A4B");
+	}
+
+	@Test
+	public void shouldReturn2A2B() throws Exception {
+		GuessNumber guessNumber = new GuessNumber("1234");
+		String result = guessNumber.validate("1243");
+		assertEquals(result, "2A2B");
+	}
+
+	@Test
+	public void shouldReturn4A0B() throws Exception {
+		GuessNumber guessNumber = new GuessNumber("1234");
+		String result = guessNumber.validate("1234");
+		assertEquals(result, "4A0B");
+	}
+}
